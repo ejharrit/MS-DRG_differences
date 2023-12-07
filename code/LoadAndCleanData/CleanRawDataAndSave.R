4623d6f483d9323a758234f8be01d1347e361286
ejharrit <ejharrit@gmail.com>
2023-12-07 09:56:46 -0800

    edits the LoadDataFromAPI.R file and adds a CleanRawDataAndSave.R file. Also adds a data dictionairy

---
+++ code/LoadAndCleanData/CleanRawDataAndSave.R
@@ -0,0 +1,99 @@
+#############################################################################################################################
+#############################################################################################################################
+##                                                                                                                         ##
+##                              This file loads the data we pulled from the Data.CMS.gov                                   ##
+##                                  API, cleans it and saves the new data format                                           ##
+##                                                                                                                         ##
+##                                                                                                                         ##
+#############################################################################################################################
+
+
+##### Load the necessary packages
+#####
+
+library(tidyverse)
+library(stringr)
+library(skimr)
+
+##### --
+#####
+
+
+##### Load the data saved from the LoadDataFromApi.R file
+#####
+
+ipps_data <- read_csv("data/raw/ipps_data.csv")
+
+##### --
+#####
+
+
+##### Check the general structure of the data to see if there are any structural issues, 
+##### such as missing values or anything like that, that need to be fixed
+#####
+
+skim(ipps_data)
+
+str(ipps_data)
+
+### Data was meant to be pulled only for California. This checks that only California data was pulled
+nrow(ipps_data) == ipps_data %>% filter(rndrng_prvdr_state_abrvtn == "CA") %>% nrow()
+
+##### --
+#####
+
+
+##### Rename and select variables and create a new variable indicating which cities are included in the Inland Empire
+##### as well as indicate whether there were complications
+#####
+
+### This uses a list of cities from the Municipal Management Association of Southern California to determine which cities 
+### are part of the Inland Empire, this list can be accessed at https://mmasc.org/163/Inland-Empire-Region
+inland_empire <- c("Adelanto",      "Apple Valley",   "Banning",        "Barstow",          "Beaumont",         
+                   "Big Bear Lake", "Calimesa",       "Canyon Lake",    "Chino",            "Chino Hills",    
+                   "Colton",        "Corona",         "Eastvale",       "Fontana",          "Grand Terrace",  
+                   "Hemet",         "Hesperia",       "Highland",       "Jurupa Valley",    "Lake Elsinore",  
+                   "Loma Linda",    "Menifee",        "Montclair",      "Moreno Valley",    "Murrieta",      
+                   "Norco",         "Ontario",        "Perris",         "Rancho Cucamonga", "Redlands",
+                   "Rialto",        "Riverside",      "San Bernardino", "San Jacinto",      "Temecula",
+                   "Upland",        "Victorville",    "Wildomar",       "Yucaipa")
+
+ipps_data <- ipps_data %>%
+  rename(
+    ### New Name  ### Original Name
+    prvdr_cd    = rndrng_prvdr_ccn,
+    prvdr_name  = rndrng_prvdr_org_name,
+    city        = rndrng_prvdr_city,
+    zip         = rndrng_prvdr_zip5
+  ) %>%
+  select(c(prvdr_cd:city, zip, drg_cd:year)) %>%
+  mutate(
+    inland_empire = if_else(
+      city %in% inland_empire,
+      1,
+      0
+    ),
+    complication = case_when(
+      str_detect(drg_desc, "WITH CC")   ~ "complication",
+      str_detect(drg_desc, "WITH MCC")  ~ "major complication",
+      TRUE                              ~ "none detected"
+    )
+  ) %>%
+  ### Reorder the columns so that year is first and the inland_empire column is near the other 
+  ### geography fields and the complication column is near the drg_desc column
+  select(year, prvdr_cd:zip, inland_empire, drg_cd:drg_desc, complication, everything())
+
+##### --
+#####
+
+##### Write the data to the clean data folder and clear the work space
+#####
+
+### write the dataframe to the clean data file as a csv
+write_csv(ipps_data, file = "data/clean/ipps_data.csv")
+
+### clear the workspace
+remove(ipps_data, inland_empire)
+
+##### --
+#####
--- code/LoadAndCleanData/LoadDataFromAPI.R
+++ code/LoadAndCleanData/LoadDataFromAPI.R
@@ -22,11 +22,11 @@ library(httr2)
 ##### GET the data from the API for the years 2019, 2018 and 2017 and limited to California records
 #####
 
-### concatenate the api urls for each year
+### concatenate the api urls for each year                                             ↓ add a filter for CA
 urls <- c(
-  "https://data.cms.gov/data-api/v1/dataset/337617e5-c18b-4cb9-8152-34851747584b/data?filter[Rndrng_Prvdr_Geo_Cd]=06",
-  "https://data.cms.gov/data-api/v1/dataset/25b5b6c9-977d-4f7a-aaa7-7dde72f3d5cc/data?filter[Rndrng_Prvdr_Geo_Cd]=06",
-  "https://data.cms.gov/data-api/v1/dataset/7c638c5f-7e8c-4346-8a25-9b37a0ed36dd/data?filter[Rndrng_Prvdr_Geo_Cd]=06"
+  "https://data.cms.gov/data-api/v1/dataset/6f6d93e1-ecf8-4b93-9845-091faf20f274/data?filter[Rndrng_Prvdr_State_FIPS]=06",
+  "https://data.cms.gov/data-api/v1/dataset/09c12f06-e3fe-4cb0-81e9-945f2078c1df/data?filter[Rndrng_Prvdr_State_FIPS]=06",
+  "https://data.cms.gov/data-api/v1/dataset/b61ba5eb-021b-4510-947e-0f198982b0e8/data?filter[Rndrng_Prvdr_State_FIPS]=06"
 )
 
 ### concatenate the years
@@ -35,28 +35,30 @@ years <- c("2019", "2018", "2017")
 
 ### Create a function that GETs the data from the url and parses it into a dataframe
 get_content <- function(url, year) {
-  request(url) %>%
-    req_perform() %>%
-    resp_body_json(simplifyVector = TRUE) %>%
+  request(urls[3]) %>%
+    ### the requests seem to only pull 1000 records, this adds an iterative request to *repeatedly* pull
+    ### 1000 records at a time
+    req_perform_iterative(next_req = iterate_with_offset("offset", start = 1, offset = 1000)) %>%
+    map_dfr(resp_body_json, simplifyVector = TRUE) %>%
     as_tibble() %>%
     clean_names() %>%
     ### add a column indicating what year the data is from
     mutate(year = year)
 }
 
-### map over our concatenated urls to pull all the data and combine it into one single data frame
+### map over the concatenated urls to pull all the data and combine it into one single data frame
 ipps_data <- map2_dfr(urls, years, get_content)
 
 ##### --
 #####
 
-##### Finally we write our data to our raw data folder and clear our work space
+##### Write the data to the raw data folder and clear the work space
 #####
 
-### write the dataframe to our raw data file as a csv
+### write the dataframe to the raw data file as a csv
 write_csv(ipps_data, file = "data/raw/ipps_data.csv")
 
-### clear our workspace
+### clear the workspace
 remove(ipps_data, urls, years, get_content)
 
 ##### --
